78357542405c64fbcdfdfcbd89e866bfe22cd6a8
kuponasourcecodecustoms <42140211+kuponasourcecodecustoms@users.noreply.github.com>
2024-05-06 16:46:50 +0100

    Installing eslint - will fix config later

---
+++ countryagroweb/eslint.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+
+   {
+       rules: {
+           "no-unused-vars": "warn",
+           "no-undef": "warn"
+       }
+   }
+];
