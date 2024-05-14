fc6103a36505e81c8a4e97d0a03880c0edaeacab
kuponasourcecodecustoms <42140211+kuponasourcecodecustoms@users.noreply.github.com>
2024-05-14 21:14:29 +0100

    Adding icons to contact card

--- countryagroweb/src/components/contactCard.js
+++ countryagroweb/src/components/contactCard.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './contactCard.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBriefcase, faEnvelope, faPhone, faUser} from '@fortawesome/free-solid-svg-icons'
 
 // eslint-disable-next-line react/prop-types
 export const ContactCard = ({name,emailAddress,contactNumber,role}) => (
     <div className='contactCard'>
-    <span>Name: {name}</span>
-    <span>Role: {role}</span>
-    <span>Email Address:
+    <span><FontAwesomeIcon icon={faUser}/>  Name: {name}</span>
+    <span><FontAwesomeIcon icon={faBriefcase}/>  Role: {role}</span>
+    <span><FontAwesomeIcon icon={faEnvelope}/>  Email Address:&nbsp;
     <Link to='javascript:void(0)'
       onClick={() => window.location = `mailto:${emailAddress}`}>
-       {emailAddress}
+        {emailAddress}
     </Link>
     </span>
-    <span>Contact Number: {contactNumber}</span>
+    <span><FontAwesomeIcon icon={faPhone}/>  Contact Number: {contactNumber}</span>
     </div>
 )
 
