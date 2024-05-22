8b173fbc55d9c47a4b2ae2d437983b27c112ccbb
kuponasourcecodecustoms <42140211+kuponasourcecodecustoms@users.noreply.github.com>
2024-05-22 14:52:24 +0100

    Map rendering with office location - need to see what else to add

--- countryagroweb/src/components/officeMap.js
+++ countryagroweb/src/components/officeMap.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 import Map from 'react-map-gl';
+import mapboxgl from '!mapbox-gl'
 import './officeMap.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 // eslint-disable-next-line react/prop-types
 const AnyReactComponent = ({ text }) => <h1>{text}</h1>
 
-export const OfficeMap = () => {
+mapboxgl.accessToken = 'pk.eyJ1IjoibmlnZWx2ZXJlIiwiYSI6ImNsd2h1b2p2OTBqN2wycXBmcm04NHl6N2gifQ.Sh6lEdLP_y1uOhGxTQCBXQ'
 
-   {/* <AnyReactComponent
-          lat={-17.8361629}
-          lng={31.1040652}
-          text="My Marker"
-        /> */}
+export const OfficeMap = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(31.056858);
+  const [lat, setLat] = useState(-17.8361629);
+  const [zoom, setZoom] = useState(12);
+
+
+  useEffect(() => {
+    if (map.current) {
+      const startMarker = new mapboxgl.Marker().setLngLat([31.1040652,-17.8361629]).addTo(map.current)
+      return
+    }
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    })
+  })
 
   return (
- 
-      <Map
-    mapLib={import('mapbox-gl')}
-    initialViewState={{
-      longitude:  -17.816638,
-      latitude: 31.056858,
-      zoom: 3.5
-    }}
-    style={{width: 600, height: 400}}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
-  />
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 
-  )
 }
