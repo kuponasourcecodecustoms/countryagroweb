8749af7fd7fe3dcb74a13629e71852d0d54c10ca
kuponasourcecodecustoms <42140211+kuponasourcecodecustoms@users.noreply.github.com>
2024-05-22 18:13:15 +0100

    Fixing linitng errors

--- countryagroweb/src/components/contactCard.js
+++ countryagroweb/src/components/contactCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './contactCard.css'
-import PropTypes from 'prop-types'
+import { string} from 'prop-types'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase, faEnvelope, faPhone, faUser} from '@fortawesome/free-solid-svg-icons'
 import whatsappLogo from '../images/whatsapp-icon-logo.png'
 
-// eslint-disable-next-line react/prop-types
-export const ContactCard = ({name,emailAddress,contactNumber,role}) => (
+export const ContactCard = ({name,emailAddress,contactNumber,role}) => {
+  const formattedNumber = contactNumber.replace(/\s/g, '')
+  return (
   <div className='contactCard'>
     <span><FontAwesomeIcon icon={faUser}/>  Name: {name}</span>
     <span><FontAwesomeIcon icon={faBriefcase}/>  Role: {role}</span>
@@ -18,15 +19,15 @@ export const ContactCard = ({name,emailAddress,contactNumber,role}) => (
       </Link>
     </span>
     <span className='contactNumber'><FontAwesomeIcon icon={faPhone}/>  Contact Number: {contactNumber} <Link to='javascript:void(0)'
-        onClick={() => window.location = `https://wa.me/${contactNumber.replace(/\s/g, '')}`}>
+        onClick={() => window.location = `https://wa.me/${formattedNumber}`}>
           <img src={whatsappLogo}/>
       </Link></span>
   </div>
-)
+)}
 
-ContactCard.PropTypes = {
-  name: PropTypes.string.isRequired,
-  emailAddress: PropTypes.string.isRequired,
-  contactNumber: PropTypes.string.isRequired,
-  role: PropTypes.string.isRequired
+ContactCard.propTypes = {
+  name: string.isRequired,
+  emailAddress: string.isRequired,
+  contactNumber: string.isRequired,
+  role: string.isRequired
 }
